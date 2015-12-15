34e3d9ef6568753b16e7dc79fba34ce5dc6227ff
McLovie <lovie520@hotmail.com>
2015-12-14 20:58:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var config = {
   app_name: 'BTCParadise',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
-  redirect_uri: 'https://untitled-dice.github.io',
+  recaptcha_sitekey: '6LfsGhMTAAAAAPIiRs4WjTs72uMM4Xxi3F5IXZDo',  // <----- EDIT ME!
+  redirect_uri: 'https://btcparadise.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
